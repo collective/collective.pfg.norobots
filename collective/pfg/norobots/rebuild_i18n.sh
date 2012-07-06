67eab219a04601f097feefedb4ad9675da38eceb
Sylvain Boureliou <sylvain.boureliou@gmail.com>
2012-07-07 00:19:36 +0200

    Fix i18n and add french translation

--- collective/pfg/norobots/rebuild_i18n.sh
+++ collective/pfg/norobots/rebuild_i18n.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Synchronise the .pot with the templates.
-i18ndude rebuild-pot --pot locales/collective.pfg.norobots.pot --create collective.pfg.norobots .
+i18ndude rebuild-pot --pot locales/collective.pfg.norobots.pot --merge locales/manual.pot --create collective.pfg.norobots .
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/collective.pfg.norobots.pot locales/*/LC_MESSAGES/collective.pfg.norobots.po
